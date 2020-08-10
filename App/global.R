29b32aa14c9814df738ab4a4e4abfb2be4e7b8b7
bberger62 <60114902+bberger62@users.noreply.github.com>
2020-08-09 21:17:29 -0400

    Update global.R

--- App/global.R
+++ App/global.R
@@ -2,6 +2,7 @@ library(shiny)
 library(shinydashboard)
 library(plotly)
 library(shinythemes)
+library(tidyverse)
 merge4 <- readr::read_csv("./merge4.csv")
 mapdata = merge4 %>%
   group_by(Country.Name, Country.Code, year) %>%
